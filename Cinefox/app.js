299590c2cbb6baa9290772ee11e1535861f01fe7
mspix <dev.mike.fh@gmail.com>
2015-05-20 01:42:55 +0200

    Bugfix

--- Cinefox/app.js
+++ Cinefox/app.js
@@ -4,7 +4,19 @@ var jsonParser = bodyParser.json();
 
 var app = express();
 
-
+var kinos = [
+  {
+    "Kino-ID" : 1,
+    "Bezeichnung" : "Cinedom",
+    "Straße" : "im MediaPark 1",
+    "PLZ" : "50670 Köln",
+    "Öffnungszeiten" : "Täglich von 10:30 - 23:30 Uhr",
+    "Reservierungs-Hotline" : "0221 - 95195555",
+    "Filmangebot" : [1,2,3],
+    "Parkplätze" : "Ja",
+    "Bewertung" : ""
+  }
+]
 
 var filme = [
               {
@@ -20,17 +32,27 @@ var filme = [
               }
           ];
 
+app.get('/', function(req, res){
+  res.send('Welcome to Cinefox!');
+});
 
+app.get('/kinos', function(req, res){
+  res.status(200).json(data);
+});
 
-
-app.post('/filme', jsonParser, function(req, res){
-    filme.push(req.body);
-    res.type('plain').send('Film Added!')
-
+app.post('/kinos', jsonParser, function(req, res){
+  data.push(req.body);
+  res.type('plain').send('Added!')
 });
 
 app.get('/filme', function(req, res){
   res.status(200).json(filme);
+
+});
+
+app.post('/filme', jsonParser, function(req, res){
+    filme.push(req.body);
+    res.type('plain').send('Film Added!')
 
 });
 
