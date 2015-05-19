cb6dfe455858ebbfd563d39255c4ce116c7cd00b
Bruce Wayne <brucewayne@batcave.com>
2015-05-19 16:46:10 +0200

    2.Commit des Tages - Clean
    Cleaning

--- Cinefox/app.js
+++ Cinefox/app.js
@@ -4,25 +4,24 @@ var jsonParser = bodyParser.json();
 
 var app = express();
 
-var data = [
-  {"title" : "Hello World!"},
-  {"description" : "This is a test."}
-]
 
-var filme = [];
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
-});
+var filme = [
+              {
+                  "id"                  :     "1",
+                  "Titel"               :     "",
+                  "Erscheinungstermin"  :     "",
+                  "Genre"               :     "",
+                  "Regie"               :     "",
+                  "Bewertung"           :     "",
+                  "Thumbnail"           :     "",
+                  "Filmbeschreibung"    :     "",
+                  "Dauer"               :     ""
+              }
+          ];
 
-app.get('/data', function(req, res){
-  res.status(200).json(data);
-});
 
-app.post('/data', jsonParser, function(req, res){
-  data.push(req.body);
-  res.type('plain').send('Added!')
-});
+
 
 app.post('/filme', jsonParser, function(req, res){
     filme.push(req.body);
@@ -33,5 +32,6 @@ app.post('/filme', jsonParser, function(req, res){
 app.get('/filme', function(req, res){
   res.status(200).json(filme);
 
+});
 
 app.listen(1337);
