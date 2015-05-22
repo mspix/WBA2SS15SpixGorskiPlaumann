3af3330e13e3794368cc5351bf0a47c52562872f
mspix <dev.mike.fh@gmail.com>
2015-05-22 20:48:09 +0200

    Users-Ressourcen und -Methoden (Nutzung von Redis)
    Users - Methoden:
    GET auf users/id
    GET auf users
    POST auf users
    PUT durch users/id
    DELETE durch users/id
    
    Gitignore mit "redis"-Eintrag ergänzt

--- Cinefox/app.js
+++ Cinefox/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
+var redis = require('redis');
 
+var db = redis.createClient();
 var app = express();
+app.use(bodyParser.json());
 
 var kinos = [
   {
@@ -16,7 +18,7 @@ var kinos = [
     "Parkplätze" : "Ja",
     "Bewertung" : ""
   }
-]
+];
 
 var filme = [
               {
@@ -32,6 +34,94 @@ var filme = [
               }
           ];
 
+
+app.post('/users', function(req, res){
+  var newUser = req.body;
+
+  db.incr('id:users', function(err, rep){
+
+    newUser.id = rep;
+
+    db.set('user:'+newUser.id, JSON.stringify(newUser), function(err, rep){
+      res.json(newUser);
+    db.bgsave();
+
+    });
+  });
+});
+
+
+app.get('/users/:id', function(req, res){
+  db.get('user:'+req.params.id, function(err, rep){
+
+    if(rep){
+      res.type('json').send(rep);
+    }
+    else{
+      res.status(404).type('text').send('Der User mit der ID '+req.params.id+' konnte nicht gefunden werden.');
+    }
+
+  });
+});
+
+
+app.put('/users/:id', function(req, res){
+  db.exists('user:'+req.params.id, function(err, rep){
+    if (rep == 1){
+      var updatedUser = req.body;
+      updatedUser.id = req.params.id;
+      db.set('user:' + req.params.id, JSON.stringify(updatedUser), function(err, rep){
+        res.json(updatedUser);
+      });
+    }
+    else {
+      res.status(404).type('text').send('Der User mit der ID' + req.params.id +'konnte nicht gefunden werden.');
+    }
+  });
+});
+
+
+app.delete('/users/:id', function(req, res){
+  db.del('user:'+req.params.id, function(err, rep){
+    if (rep == 1){
+      res.status(200).type('text').send('OK - User gelöscht');
+    }
+    else {
+      res.status(404).type('text').send('Der User mit der ID' + req.params.id +' konnte nicht gefunden werden.');
+    }
+  });
+});
+
+
+app.get('/users', function(req, res){
+  db.keys('user:*', function(err, rep){
+
+    var users = [];
+
+    if(rep.length == 0){
+      res.json(users);
+      return;
+    }
+
+    db.mget(rep, function(err, rep){
+
+      rep.forEach(function(val){
+        users.push(JSON.parse(val));
+      });
+
+      users = users.map(function(user){
+        return {id: user.id, name: user.name};
+      });
+
+      res.json(users);
+
+    });
+  });
+});
+
+
+
+
 app.get('/', function(req, res){
   res.send('Welcome to Cinefox!');
 });
@@ -40,7 +130,7 @@ app.get('/kinos', function(req, res){
   res.status(200).json(data);
 });
 
-app.post('/kinos', jsonParser, function(req, res){
+app.post('/kinos', function(req, res){
   data.push(req.body);
   res.type('plain').send('Added!')
 });
@@ -50,7 +140,7 @@ app.get('/filme', function(req, res){
 
 });
 
-app.post('/filme', jsonParser, function(req, res){
+app.post('/filme', function(req, res){
     filme.push(req.body);
     res.type('plain').send('Film Added!')
 
