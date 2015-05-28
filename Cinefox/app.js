289e6e0c5631578fcacf1511471396b6f9fa763a
mspix <dev.mike.fh@gmail.com>
2015-05-28 10:04:05 +0200

    Filmformular
    W.I.P. - Ist noch buggy
    
    - Resource zur vereinfachten Eingabe von Filmen in die Datenbank
    (/filmForm)
    - filmForm: 2 Methoden: GET & POST
    
    app.use(bodyParser.json()); wird nicht mehr genutzt.
    Parser werden je nach Notwendigkeit explizit angegeben.
    
    .gitignore ergänzt um einen Eintrag (dump.rdb)

--- Cinefox/app.js
+++ Cinefox/app.js
@@ -1,41 +1,45 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 var redis = require('redis');
 
 var db = redis.createClient();
 var app = express();
-app.use(bodyParser.json());
 
-var kinos = [
-  {
-    "Kino-ID" : 1,
-    "Bezeichnung" : "Cinedom",
-    "Straße" : "im MediaPark 1",
-    "PLZ" : "50670 Köln",
-    "Öffnungszeiten" : "Täglich von 10:30 - 23:30 Uhr",
-    "Reservierungs-Hotline" : "0221 - 95195555",
-    "Filmangebot" : [1,2,3],
-    "Parkplätze" : "Ja",
-    "Bewertung" : ""
-  }
-];
-
-var filme = [
-              {
-                  "id"                  :     "1",
-                  "Titel"               :     "",
-                  "Erscheinungstermin"  :     "",
-                  "Genre"               :     "",
-                  "Regie"               :     "",
-                  "Bewertung"           :     "",
-                  "Thumbnail"           :     "",
-                  "Filmbeschreibung"    :     "",
-                  "Dauer"               :     ""
-              }
-          ];
+// app.use(bodyParser.json());
+// app.use(bodyParser());
 
 
-app.post('/users', function(req, res){
+// var kinos = [
+//   {
+//     "Kino-ID" : 1,
+//     "Bezeichnung" : "Cinedom",
+//     "Straße" : "im MediaPark 1",
+//     "PLZ" : "50670 Köln",
+//     "Öffnungszeiten" : "Täglich von 10:30 - 23:30 Uhr",
+//     "Reservierungs-Hotline" : "0221 - 95195555",
+//     "Filmangebot" : [1,2,3],
+//     "Parkplätze" : "Ja",
+//     "Bewertung" : ""
+//   }
+// ];
+//
+// var filme = [
+//               {
+//                   "id"                  :     "1",
+//                   "Titel"               :     "",
+//                   "Erscheinungstermin"  :     "",
+//                   "Genre"               :     "",
+//                   "Regie"               :     "",
+//                   "Bewertung"           :     "",
+//                   "Thumbnail"           :     "",
+//                   "Filmbeschreibung"    :     "",
+//                   "Dauer"               :     ""
+//               }
+//           ];
+
+
+app.post('/users', jsonParser, function(req, res){
   var newUser = req.body;
 
   db.incr('id:users', function(err, rep){
@@ -65,7 +69,7 @@ app.get('/users/:id', function(req, res){
 });
 
 
-app.put('/users/:id', function(req, res){
+app.put('/users/:id', jsonParser, function(req, res){
   db.exists('user:'+req.params.id, function(err, rep){
     if (rep == 1){
       var updatedUser = req.body;
@@ -136,14 +140,134 @@ app.post('/kinos', function(req, res){
 });
 
 app.get('/filme', function(req, res){
-  res.status(200).json(filme);
+  db.keys('film:*', function(err, rep){
 
+    var filme = [];
+
+    if(rep.length == 0){
+      res.json(filme);
+      return;
+    }
+
+    db.mget(rep, function(err, rep){
+
+      rep.forEach(function(val){
+        filme.push(JSON.parse(val));
+      });
+
+      filme = filme.map(function(film){
+        return {id: film.id, titel: film.titel};
+      });
+
+      res.json(filme);
+
+    });
+  });
 });
 
 app.post('/filme', function(req, res){
     filme.push(req.body);
     res.type('plain').send('Film Added!')
 
+});
+
+
+// A browser's default method is 'GET', so this
+// is the route that express uses when we visit
+// our site initially.
+app.get('/filmForm', function(req, res){
+  // The form's action is '/' and its method is 'POST',
+  // so the `app.post('/', ...` route will receive the
+  // result of our form
+  var html =  '<h1>Filmformular</h1><br>' +
+              '<form action="/filmForm" method="post">' +
+                 'Titel: ' +
+                 '<input type="text" name="title" />' +
+                 '<br>' +
+                 'Erscheinungstermin: ' +
+                 '<input type="text" name="relDate" />' +
+                 '<br>' +
+                 'Genre: ' +
+                 '<input type="text" name="genre" />' +
+                 '<br>' +
+                 'Regie: ' +
+                 '<input type="text" name="regie" />' +
+                 '<br>' +
+                 'Bewertung: ' +
+                 '<input type="text" name="evaluation" />' +
+                 '<br>' +
+                 'Thumbnail: ' +
+                 '<input type="text" name="thumbnail" />' +
+                 '<br>' +
+                 'Beschreibung: ' +
+                 '<input type="text" name="description" />' +
+                 '<br>' +
+                 'Dauer: ' +
+                 '<input type="text" name="length" />' +
+                 '<br>' +
+                 '<button type="submit">Submit</button>' +
+              '</form>';
+
+
+
+  res.send(html);
+});
+
+
+app.post('/filmForm', bodyParser(), function(req, res){
+
+  // var newFilm = {
+  //                   "titel"               :     "req.body.title"
+  //               };
+
+
+
+  // var newFilm = '{
+  //                   "titel"               :     "' + req.body.title +         '",
+  //                   "Erscheinungstermin"  :     "' + req.body.relDate +       '",
+  //                   "Genre"               :     "' + req.body.genre +         '",
+  //                   "Regie"               :     "' + req.body.regie +         '",
+  //                   "Bewertung"           :     "' + req.body.evaluation +    '",
+  //                   "Thumbnail"           :     "' + req.body.thumbnail +     '",
+  //                   "Beschreibung"        :     "' + req.body.description +   '",
+  //                   "Dauer"               :     "' + req.body.length +        '"
+  //               }';
+
+  db.incr('id:filme', function(err, rep){
+
+      // var newFilm = "{
+      //                   \"Titel\"               :     \"" + req.body.title +         "\",
+      //                   \"Erscheinungstermin\"  :     \"" + req.body.relDate +       "\",
+      //                   \"Genre\"               :     \"" + req.body.genre +         "\",
+      //                   \"Regie\"               :     \"" + req.body.regie +         "\",
+      //                   \"Bewertung\"           :     \"" + req.body.evaluation +    "\",
+      //                   \"Thumbnail\"           :     \"" + req.body.thumbnail +     "\",
+      //                   \"Beschreibung\"        :     \"" + req.body.description +   "\",
+      //                   \"Dauer\"               :     \"" + req.body.length +        "\"
+      //               }";
+      var newFilm = {
+                        "titel"               :     req.body.title,
+                        "erscheinungstermin"  :     req.body.relDate,
+                        "genre"               :     req.body.genre,
+                        "regie"               :     req.body.regie,
+                        "bewertung"           :     req.body.evaluation,
+                        "thumbnail"           :     req.body.thumbnail,
+                        "beschreibung"        :     req.body.description,
+                        "dauer"               :     req.body.length
+                    };
+
+                    newFilm.id = rep;
+
+    db.set('film:'+newFilm.id, JSON.stringify(newFilm), function(err, rep){
+      res.json(newFilm);
+    db.bgsave();
+
+    });
+
+  // var confirmationParsed = JSON.parse(newFilm);
+  });
+
+  res.send('Added!');
 });
 
 app.listen(1337);
