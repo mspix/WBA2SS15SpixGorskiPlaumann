72f769ae9a25b26608bddd67986bc0de565d9060
mspix <dev.mike.fh@gmail.com>
2015-06-26 02:45:32 +0200

    Dienstnutzer - Erste Schritte

---
+++ Cinefox/dienstnutzer.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var redis = require('redis');
+
+var db = redis.createClient();
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/users', function(req, res){
+  var newUser = req.body;
+
+  db.incr('id:users', function(err, rep){
+
+    newUser.id = rep;
+
+    db.set('user:'+newUser.id, JSON.stringify(newUser), function(err, rep){
+      res.json(newUser);
+    db.bgsave();
+
+    });
+  });
+});
+
+
+app.get('/users/:id', function(req, res){
+  db.get('user:'+req.params.id, function(err, rep){
+
+    if(rep){
+      res.type('json').send(rep);
+    }
+    else{
+      res.status(404).type('text').send('Der User mit der ID '+req.params.id+' konnte nicht gefunden werden.');
+    }
+
+  });
+});
+
+
+app.put('/users/:id', function(req, res){
+  db.exists('user:'+req.params.id, function(err, rep){
+    if (rep == 1){
+      var updatedUser = req.body;
+      updatedUser.id = Number(req.params.id); // Ohne Number() würde die ID zu einem String gewandelt werden
+      db.set('user:' + req.params.id, JSON.stringify(updatedUser), function(err, rep){
+        res.json(updatedUser);
+      });
+    }
+    else {
+      res.status(404).type('text').send('Der User mit der ID' + req.params.id +'konnte nicht gefunden werden.');
+    }
+  });
+});
+
+
+app.delete('/users/:id', function(req, res){
+  db.del('user:'+req.params.id, function(err, rep){
+    if (rep == 1){
+      res.status(200).type('text').send('OK - User gelöscht');
+    }
+    else {
+      res.status(404).type('text').send('Der User mit der ID' + req.params.id +' konnte nicht gefunden werden.');
+    }
+  });
+});
+
+app.get('/users', function(req, res){
+
+		var users = [];
+
+		db.keys('user:*', function(err, rep){
+
+
+			if(rep.length == 0){
+			  res.json(users);
+			  return;
+			}
+			
+			db.mget(rep, function(err, rep){
+
+				rep.forEach(function(val){
+					users.push(JSON.parse(val));
+				});
+			
+				res.json(queryFilter(kinos, req.query));
+			
+			});
+		});
+		
+
+});
+
+
+app.get('/', function(req, res){
+  res.send('Welcome to Cinefox!');
+});
+
+
+app.listen(1338);
